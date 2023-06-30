2a57e15e709eece6a5bafdf2447d32d75df577ad
Bruno Roque <brunoroque06@gmail.com>
2023-06-30 23:06:50 +0200

    Website has a header and a main, no card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,46 @@
 import { Component } from "@angular/core";
 import { concatMap, delay, from, of } from "rxjs";
-import { AsyncPipe } from "@angular/common";
-import { LogosComponent } from "./logos.component";
+import { AsyncPipe, NgForOf } from "@angular/common";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [AsyncPipe, LogosComponent],
+  imports: [AsyncPipe, NgForOf],
   template: `
     <header>
-      <div class="name">
+      <a
+        *ngFor="let l of links"
+        href="{{ l.ref }}"
+        target="_blank"
+        rel="noopener noreferrer"
+        >{{ l.name }}</a
+      >
+    </header>
+    <main>
+      <div>
         <span>{{ name | async }}</span>
-        <span class="caret">#</span>
+        <span class="caret">&nbsp;</span>
       </div>
       <div class="title">Software Engineer</div>
-      <app-logos></app-logos>
-    </header>
+    </main>
   `,
 })
 export class AppComponent {
+  links = [
+    {
+      name: "Curriculum Vitae",
+      ref: "/assets/docs/bruno-roque-cv.pdf",
+    },
+    {
+      name: "GitHub",
+      ref: "https://github.com/brunoroque06",
+    },
+    {
+      name: "LinkedIn",
+      ref: "https://www.linkedin.com/in/brunoroque06",
+    },
+  ];
+
   name = of("Bruno Roque").pipe(
     delay(1000),
     concatMap((n) =>

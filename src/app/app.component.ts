0cdbdf3651c4bd57e37353bfbc999ce1ccdc4fd7
Bruno Roque <brunoroque06@gmail.com>
2023-07-23 21:59:42 +0200

    Name comes first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,13 @@ import { AsyncPipe, NgForOf } from "@angular/common";
   imports: [AsyncPipe, NgForOf],
   template: `
     <header>
+      <div>
+        <span>{{ name | async }}</span>
+        <span class="caret">&nbsp;</span>
+      </div>
+      <div class="title">Software Engineer</div>
+    </header>
+    <main>
       <a
         *ngFor="let l of links"
         href="{{ l.ref }}"
@@ -15,17 +22,18 @@ import { AsyncPipe, NgForOf } from "@angular/common";
         rel="noopener noreferrer"
         >{{ l.name }}</a
       >
-    </header>
-    <main>
-      <div>
-        <span>{{ name | async }}</span>
-        <span class="caret">&nbsp;</span>
-      </div>
-      <div class="title">Software Engineer</div>
     </main>
   `,
 })
 export class AppComponent {
+  name = of("Bruno Roque").pipe(
+    delay(1000),
+    concatMap((n) =>
+      from([...Array(n.length + 1).keys()].map((i) => n.substring(0, i))),
+    ),
+    concatMap((n) => of(n).pipe(delay(100))),
+  );
+
   links = [
     {
       name: "Curriculum Vitae",
@@ -40,12 +48,4 @@ export class AppComponent {
       ref: "https://www.linkedin.com/in/brunoroque06",
     },
   ];
-
-  name = of("Bruno Roque").pipe(
-    delay(1000),
-    concatMap((n) =>
-      from([...Array(n.length + 1).keys()].map((i) => n.substring(0, i)))
-    ),
-    concatMap((n) => of(n).pipe(delay(100)))
-  );
 }
